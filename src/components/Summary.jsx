1c327940bf09ab3279bfcc50472b51d5c8cdf5a2
Woonhian <limwoonhian@hotmail.com>
2020-07-13 22:54:49 +0800

    Summary component

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ExpenseContext } from "../contexts/expenseContext";
 
 const Summary = () => {
+  const { entries } = useContext(ExpenseContext);
+  let income = 0;
+  let expense = 0;
+  entries.forEach((el) => {
+    if (el.amount > 0) {
+      income += parseInt(el.amount);
+    } else {
+      expense += parseInt(el.amount);
+    }
+  });
   return (
     <div className="row">
       <div className="col-sm-6">
         <div className="card border-success">
           <div className="card-body">
             <h5 className="card-title text-center">Income</h5>
-            <h4 className="text-center text-success display-5">+$100</h4>
+            <h4 className="text-center text-success display-5">+${income}</h4>
           </div>
         </div>
       </div>
@@ -15,7 +26,9 @@ const Summary = () => {
         <div className="card border-danger">
           <div className="card-body">
             <h5 className="card-title text-center">Expenses</h5>
-            <h4 className="text-center text-danger display-5">-$100</h4>
+            <h4 className="text-center text-danger display-5">
+              -${Math.abs(expense)}
+            </h4>
           </div>
         </div>
       </div>
