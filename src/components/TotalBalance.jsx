51be79cc3f87eb13027029e01c32faf9aa8252d1
Woonhian <limwoonhian@hotmail.com>
2020-07-13 21:59:15 +0800

    TotalBalance component

--- src/components/TotalBalance.jsx
+++ src/components/TotalBalance.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ExpenseContext } from "../contexts/expenseContext";
 
 const TotalBalance = () => {
+  // Retrive state
+  const { entries } = useContext(ExpenseContext);
+  let sum = 0;
+  entries.forEach((el) => {
+    sum += parseInt(el.amount);
+  });
+
   return (
     <div>
-      <h2 className="display-3 text-center">$200</h2>
+      <h2 className="display-3 text-center">${sum}</h2>
     </div>
   );
 };
